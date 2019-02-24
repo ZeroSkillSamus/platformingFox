3293c26a084376b7766b7fef5622afb6e8c97207
ZeroSkillSamus <amitchell2@albany.edu>
2019-02-24 11:05:09 -0500

    played around with the physics
    changed the deault from (0,-9.81,0) to (0,-10)

---
+++ platformerUnity2D/Platformer Fox/Assets/Scripts/ButtonScripts/PlayButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PlayButton : MonoBehaviour {
+	public GameObject Panel;
+	public GameObject Buttons;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void getRidOf()
+	{
+		Panel.SetActive (true);
+		Buttons.SetActive (false);
+	}
+
+	public void backToMainMenu()
+	{
+		Panel.SetActive (false);
+		Buttons.SetActive (true);
+	}
+
+	public void startLevel1()
+	{
+		SceneManager.LoadScene (1);
+	}
+}
